e68a9be7852b2730d368da27ebc8c5f1ea21ca02
Dikshant Solanki <dcode2100@gmail.com>
2023-01-21 22:39:51 +0530

    Home1

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,19 +4,25 @@ import './Home.css';
 
 const Home = () => {
   return (
-    <div className='home'>
-        <Header/>
-        <div className='home__container'>
-            <div className="home_welcome-container">
-                <div className="home_welcome-container-left">
-                  containerleft
-                </div>
-                <div className="home_welcome-container-right">
-                  containerright
-                </div>
-            </div>
+    <div className="home">
+      <Header />
+      <div className="home__container">
+        <div className="home_welcome-container">
+          <div className="home_welcome-container-left">
+            <h1 className="Wel">Welcome to CAEDES</h1>
+            <h1 className="weprovide">We provide</h1>
+            <span className='slogen'>
+              we are the keychain experts, satisfying customers looking for a
+              keychain for Gifting, or a partner for your keys our catelog is
+              filled with great designs and the highest quality at the best
+              price.
+            </span>
+            <button className="btn1">SHOP NOW</button>
+          </div>
+          <div className="home_welcome-container-right">containerright</div>
         </div>
-        <Footer/>
+      </div>
+      <Footer />
     </div>
   );
 }
--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import { Footer, Header } from '../../Components';
 import "./Shop.css";
 
+
 const Shop = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => (
+    fetch
+  ))
   return (
     <div className="shop">
       <Header />
